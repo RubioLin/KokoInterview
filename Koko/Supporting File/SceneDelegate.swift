f49adbb7bb4cb4fdac5df4714d4b362b68271f13
RubioLin <capricorn1230maple@gmail.com>
2024-06-03 16:09:13 +0800

    feat: add images and fonts and update app entry

--- Koko/Supporting File/SceneDelegate.swift
+++ Koko/Supporting File/SceneDelegate.swift
@@ -7,7 +7,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        
+        let navi = UINavigationController(rootViewController: SelectStateViewController())
+        window?.rootViewController = navi
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
